b976caffa678d00a0e6c01fea324070ea2eac414
umair582 <it26101501@my.sliit.lk>
2026-09-13 20:07:34 +0530

    Add files via upload

---
+++ Q2/lab7q2a.java
@@ -0,0 +1,14 @@
+class lab7q2a
+{
+	public static void main(String args[])
+
+	{
+	 for(int row=1;row<5;row++)
+	 {
+		 for(int cl=1;cl<6;cl++)
+		 { System.out.print("$");}
+		 
+		 System.out.println();
+	 }
+	}
+}
---
+++ Q2/lab7q2c.java
@@ -0,0 +1,15 @@
+class lab7q2c
+{
+	public static void main(String args[])
+	{
+		for(int row=5;row>=1;row--)
+		{
+			for(int cl=1;cl<=row;cl++)
+			{
+			  System.out.print(row);
+		     }
+			 System.out.println();
+		}
+		
+	}
+}
